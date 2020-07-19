84bddac266ca8d028fafe91119708874634ccc68
Shinya Ohyanagi <heavenshell.jp@gmail.com>
2020-07-19 15:14:01 +0900

    Fix follow latest storybook conifg (#60)

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: ['../src/**/*.stories.ts'],
+  addons: [
+    '@storybook/addon-actions/register',
+    '@storybook/addon-links/register',
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-viewport/register',
+  ],
+}
