da176bbf5caaa99c8d2c9301a03c1207c5c5fa3e
Shinya Ohyanagi <heavenshell.jp@gmail.com>
2020-12-22 00:15:10 +0900

    Fix

--- src/app/components/pages/launchpads/launchpad-list/launchpad-list.component.spec.ts
+++ src/app/components/pages/launchpads/launchpad-list/launchpad-list.component.spec.ts
@@ -1,5 +1,9 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { LayoutModule } from 'src/app/components/templates/layout/layout.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { LaunchpadListComponent } from './launchpad-list.component';
 
 describe('LaunchpadListComponent', () => {
@@ -8,6 +12,12 @@ describe('LaunchpadListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        SharedModule,
+        LayoutModule,
+      ],
       declarations: [LaunchpadListComponent],
     }).compileComponents();
   });
